f723c0800debd506ae319dd2c1c681491d9c2ea0
bingo <2577200835@qq.com>
2021-07-24 19:28:29 +0800

    [update]更新rtthread内核到3.1.5

--- USER/board.c
+++ USER/board.c
@@ -1,15 +1,25 @@
-/* 开发板硬件相关头文件 */
-#include "board.h" 
+/*
+ * Copyright (c) 2006-2019, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2021-05-24                  the first version
+ */
 
-/* RT-Thread相关头文件 */
 #include <rthw.h>
 #include <rtthread.h>
-
+#include "board.h"
 
 #if defined(RT_USING_USER_MAIN) && defined(RT_USING_HEAP)
-#define RT_HEAP_SIZE 1024
-/* 从内部SRAM里面分配一部分静态内存来作为rtt的堆空间，这里配置为4KB */
-static uint32_t rt_heap[RT_HEAP_SIZE];
+/*
+ * Please modify RT_HEAP_SIZE if you enable RT_USING_HEAP
+ * the RT_HEAP_SIZE max value = (sram size - ZI size), 1024 means 1024 bytes
+ */
+#define RT_HEAP_SIZE (15*1024)
+static rt_uint8_t rt_heap[RT_HEAP_SIZE];
+
 RT_WEAK void *rt_heap_begin_get(void)
 {
     return rt_heap;
@@ -21,99 +31,14 @@ RT_WEAK void *rt_heap_end_get(void)
 }
 #endif
 
-/**
-  * @brief  开发板硬件初始化函数
-  * @param  无
-  * @retval 无
-  *
-  * @attention
-  * RTT把开发板相关的初始化函数统一放到board.c文件中实现，
-  * 当然，你想把这些函数统一放到main.c文件也是可以的。
-  */
-void rt_hw_board_init()
+void rt_os_tick_callback(void)
 {
-	/* 系统时钟初始化成400MHz */
-	SystemClock_Config();
-	
-    /* 初始化SysTick */
-  HAL_SYSTICK_Config( HAL_RCC_GetSysClockFreq() / RT_TICK_PER_SECOND );	
-    
-	/* 硬件BSP初始化统统放在这里，比如LED，串口，LCD等 */
-    
-	/* LED 端口初始化 */
-	LED_GPIO_Config();	
-	
-	/* usart 端口初始化 */
-  DEBUG_USART_Config();
-
-	
-/* 调用组件初始化函数 (use INIT_BOARD_EXPORT()) */
-#ifdef RT_USING_COMPONENTS_INIT
-    rt_components_board_init();
-#endif
-    
-#if defined(RT_USING_CONSOLE) && defined(RT_USING_DEVICE)
-	rt_console_set_device(RT_CONSOLE_DEVICE_NAME);
-#endif
-    
-#if defined(RT_USING_USER_MAIN) && defined(RT_USING_HEAP)
-    rt_system_heap_init(rt_heap_begin_get(), rt_heap_end_get());
-#endif
-}
-
-/**
-  * @brief  SysTick中断服务函数
-  * @param  无
-  * @retval 无
-  *
-  * @attention
-  * SysTick中断服务函数在固件库文件stm32f10x_it.c中也定义了，而现在
-  * 在board.c中又定义一次，那么编译的时候会出现重复定义的错误，解决
-  * 方法是可以把stm32f10x_it.c中的注释或者删除即可。
-  */
-void SysTick_Handler(void)
-{
-    /* 进入中断 */
     rt_interrupt_enter();
-
-    /* 更新时基 */
+    
     rt_tick_increase();
 
-    /* 离开中断 */
     rt_interrupt_leave();
 }
-
-
-
-/**
-  * @brief  重映射串口DEBUG_USARTx到rt_kprintf()函数
-  *   Note：DEBUG_USARTx是在bsp_usart.h中定义的宏，默认使用串口1
-  * @param  str：要输出到串口的字符串
-  * @retval 无
-  *
-  * @attention
-  * 
-  */
-void rt_hw_console_output(const char *str)
-{	
-	/* 进入临界段 */
-    rt_enter_critical();
-
-	/* 直到字符串结束 */
-    while (*str!='\0')
-	{
-		/* 换行 */
-        if (*str=='\n')
-		{
-//			HAL_UART_Transmit( &UartHandle,(uint8_t *)'\r',1,1000);
-		}
-			HAL_UART_Transmit( &UartHandle,(uint8_t *)(str++),1,1000);
-	}	
-
-	/* 退出临界段 */
-    rt_exit_critical();
-}
-
 
 /**
   * @brief  System Clock 配置
@@ -187,4 +112,68 @@ static void SystemClock_Config(void)
 }
 
 
-/****************************END OF FILE***************************/
+/**
+ * This function will initial your board.
+ */
+void rt_hw_board_init(void)
+{
+	SystemClock_Config();
+	/* 初始化SysTick */
+	HAL_SYSTICK_Config( HAL_RCC_GetSysClockFreq() / RT_TICK_PER_SECOND ); 
+
+	/* 硬件BSP初始化统统放在这里，比如LED，串口，LCD等 */
+
+	/* LED 端口初始化 */
+	LED_GPIO_Config();  
+
+    /* 
+     * TODO 1: OS Tick Configuration
+     * Enable the hardware timer and call the rt_os_tick_callback function
+     * periodically with the frequency RT_TICK_PER_SECOND. 
+     */
+
+    /* Call components board initial (use INIT_BOARD_EXPORT()) */
+#ifdef RT_USING_COMPONENTS_INIT
+    rt_components_board_init();
+#endif
+
+#if defined(RT_USING_USER_MAIN) && defined(RT_USING_HEAP)
+    rt_system_heap_init(rt_heap_begin_get(), rt_heap_end_get());
+#endif
+}
+
+#ifdef RT_USING_CONSOLE
+
+static int uart_init(void)
+{
+	/* usart 端口初始化 */
+	DEBUG_USART_Config();
+
+    return 0;
+}
+INIT_BOARD_EXPORT(uart_init);
+
+void rt_hw_console_output(const char *str)
+{
+		/* 进入临界段 */
+		rt_enter_critical();
+	
+		/* 直到字符串结束 */
+		while (*str!='\0')
+		{
+			/* 换行 */
+			if (*str=='\n')
+			{
+	//			HAL_UART_Transmit( &UartHandle,(uint8_t *)'\r',1,1000);
+			}
+				HAL_UART_Transmit( &UartHandle,(uint8_t *)(str++),1,1000);
+		}	
+	
+		/* 退出临界段 */
+		rt_exit_critical();
+
+}
+
+
+#endif
+
--- USER/main.h
+++ USER/main.h
@@ -3,7 +3,6 @@
 
 #include "stm32f7xx_hal.h"
 
-static void SystemClock_Config(void);
 
 #endif /* __MAIN_H */
 
