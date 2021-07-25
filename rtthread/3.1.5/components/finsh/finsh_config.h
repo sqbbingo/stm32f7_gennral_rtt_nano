4c53db3f12523c2fdd5f851d02125b38f99bf38f
bingo <2577200835@qq.com>
2021-07-25 16:04:13 +0800

    [update]开启命令回溯

--- rtthread/3.1.5/components/finsh/finsh_config.h
+++ rtthread/3.1.5/components/finsh/finsh_config.h
@@ -4,9 +4,13 @@
 #define __MSH_CFG_H__
 
 // <<< Use Configuration Wizard in Context Menu >>>
-#define RT_USING_FINSH
-#define FINSH_USING_MSH
-#define FINSH_USING_MSH_ONLY
+#define RT_USING_FINSH          //使能 FinSH
+#define FINSH_USING_MSH         //使能 msh 模式
+#define FINSH_USING_MSH_ONLY    //只使用 msh 模式
+
+#define FINSH_USING_HISTORY     //打开历史回溯功能
+// #define FINSH_USING_AUTH        //使能权限验证
+
 // <h>FinSH Configuration
 // <o>the priority of finsh thread <1-30>
 //  <i>the priority of finsh thread
